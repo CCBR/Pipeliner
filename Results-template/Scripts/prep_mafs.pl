e534349afbe4fbc660304d082bed4597d50fd77d
jlac <justin.lack@nih.gov>
2019-02-20 22:10:17 -0500

    improved somatic variant fitlering and maftools summaries

--- Results-template/Scripts/prep_mafs.pl
+++ Results-template/Scripts/prep_mafs.pl
@@ -42,7 +42,7 @@ while (<G>){
 		next if ($line[0] =~ /TTN|MUC16|OBSCN|AHNAK2|SYNE1|FLG|MUC5B|DNAH17|PLEC|DST|SYNE2|NEB|HSPG2|LAMA5|AHNAK|HMCN1|USH2A|DNAH11|MACF1|MUC17|DNAH5|GPR98|FAT1|PKD1|MDN1|RNF213|RYR1|DNAH2|DNAH3|DNAH8|DNAH1|DNAH9|ABCA13|SRRM2|CUBN|SPTBN5|PKHD1|LRP2|FBN3|CDH23|DNAH10|FAT4|RYR3|PKHD1L1|FAT2|CSMD1|PCNT|COL6A3|FRAS1|FCGBP|RYR2|HYDIN|XIRP2|LAMA1/);
 		if (($line[44] eq '-') || ($line[44] < 2)) {
 			if (($line[41] > 2) && ($line[39] > 9)) {
-				if ((($line[123] eq '-') || ($line[123] < 0.001)) && (($line[76] eq '-') || ($line[76] < 0.01)) && (($line[99] eq '-') || ($line[99] < 0.001))) {
+				if ((($line[123] eq '-') || ($line[123] < 0.001)) && (($line[76] eq '-') || ($line[76] < 0.01)) && (($line[99] eq '-') || ($line[99] < 0.001)) && (($line[41]/$line[39]) > 0.05) {
 					print C "$_\t";
 					if ($line[39] != 0){
 						$calc=($line[41]/$line[39]);
