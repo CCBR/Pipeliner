603bf223532cc60cc38b57f2f468711022ca7c2c
Vishal N Koparde <vishal.koparde@nih.gov>
2019-02-26 07:59:34 -0500

    filtersamples.R: changed .. 4th column in contrasts.tab now denotes the number of samples PER GROUP that are required to meet the CPM threshold denoted in column 3 of the same file. If number in column 4 is greater than number of samples in the group, then MINCOUNT is reset to the number of samples in the group.

--- Results-template/Scripts/filtersamples.R
+++ Results-template/Scripts/filtersamples.R
@@ -11,21 +11,36 @@ RAWCOUNTSTABLE <- args[7]
 FILTEREDRAWCOUNTSTABLE <- args[8]
 
 x=read.table(SAMPLETABLE,header = T,sep="\t")
+
 samples=(x$condition==G1 | x$condition==G2)
-
 x1=x[samples,]
-#write.table(x1,file=OUTSAMPLETABLE,sep="\t",col.names=T,quote=F,row.names = F)
 
+g1_samples=(x1$condition==G1)
+ng1=min(length(g1_samples[g1_samples==TRUE]),MINSAMPLES)
+g2_samples=(x1$condition==G2)
+ng2=min(length(g2_samples[g2_samples==TRUE]),MINSAMPLES)
 
 y=read.table(RAWCOUNTSTABLE,header = T,sep = "\t")
+y=y[,samples]
 row.names(y)=y$symbol
 y=y[,-which(names(y) %in% ("symbol"))]
-#y=y[,samples]
 y=ceiling(y)
-#colnames(y)=x1$label
-k=rowSums(cpm(y)>CPM_CUTOFF)>=MINSAMPLES
+
+y_g1=y[,g1_samples]
+y_g2=y[,g2_samples]
+# kcount_g1=rowSums(y_g1>10)>=ng1
+# kcount_g2=rowSums(y_g2>10)>=ng2
+k_g1=rowSums(cpm(y_g1)>CPM_CUTOFF)>=ng1
+k_g2=rowSums(cpm(y_g2)>CPM_CUTOFF)>=ng2
+table(k_g1)
+table(k_g2)
+# table(kcount_g1)
+# table(kcount_g2)
+k=k_g1|k_g2
+table(k)
+# k=k|kcount_g1|kcount_g2
 # table(k)
 y=y[k,]
+y=cbind(symbol=row.names(y),y)
+write.table(y,file=FILTEREDRAWCOUNTSTABLE,sep="\t",col.names=T,quote=F,row.names = F)
 
-y1=cbind(symbol=row.names(y),y)
-write.table(y1,file=FILTEREDRAWCOUNTSTABLE,sep="\t",col.names=T,quote=F,row.names = F)
